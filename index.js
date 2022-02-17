6ef0d34e79f3dc5b11056bd7938eae1c315d7f8a
kbario <kylebario1@gmail.com>
2022-02-17 13:41:36 +0800

    createHTML.js status message update

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 // require all needed packages
     // inquirer
-const inquirer = require('inquirer');
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-    // custom processing functions
-const { translateDataToObj } = require('./src/infoProcess') 
-    // custom helper
-// const { capitalise, validateEmail } = require('./src/teamGenHelper')
+const inquirer = require('inquirer'); // a npm package that allows interaction with and survey of users and returns objects containing data
+const { translateDataToObj } = require('./src/infoProcess') // custom function that translates the data retrieved from inquirer to instances of custom classes
+const { writeHtml } = require('./src/createHTML') // custom functions that take instances of custom classes, formats them and saves them in a html file
+// const { capitalise, validateEmail } = require('./src/teamGenHelper') // custom functions
 
 // import questions from questions src file
 const { questions, initial } = require('./src/questions')
 
-// create var to store recursive values
+// create const to store recursive values
 const output = [];
 
 // create function that asks dynamic questions
@@ -23,7 +19,7 @@ function asks(question){
         if(answers.role){
             if(answers.role === "No. Finalise Team."){     
                 const myObjs = translateDataToObj(output)
-                
+                writeHtml(myObjs)
                 return 
             } else {
                 questions.unshift(initial)
@@ -38,7 +34,3 @@ function asks(question){
 
 // run said function
 asks(questions)
-
-
-const obj4 = new Manager('asdf', 'asdf', 'asdf', 'asdf')
-
--- src/createHTML.js
+++ src/createHTML.js
@@ -30,7 +30,7 @@ function createHtmlContent(cards) {
         <h1 class="text-4xl text-slate-100 dark:text-slate-300 font-sans sm:text-6xl">My Team</h1>
     </header>
     <main id="container" class="flex grow p-5 gap-5 flex-wrap justify-center overflow-y-auto max-w-6xl mx-auto">
-    ${cards}     
+        ${cards}     
     </main>
     <footer class="w-full flex justify-center items-center bg-slate-600 dark:bg-slate-100 py-2">
         <h1 class="text-lg text-slate-100 dark:text-slate-600">Made with &#10084; by <a class="footer-link" href="https://www.kbar.io" target="_blank">kbar.io</a> and <a class="footer-link" href="https://github.com/kbario/TeamGener8tor" target="_blank">TeamGener8tor</a></h1>
@@ -44,8 +44,8 @@ function createHtmlContent(cards) {
 function writeHtml(objs) {
     const cards = createCards(objs);
     const file = createHtmlContent(cards)
-    fs.writeFile('../dist/index.html', file, (error) => { console.log('file was not written') });
+    fs.writeFile('../dist/index.html', file, (error) => {error ? console.log('\x1B[0;31mHTML \x1B[1;4;31munsuccessfully\x1B[0;31m saved.\x1B[0m') : console.log('\x1B[0;32mHTML \x1B[1;4;32msuccessfully\x1B[0;32m saved.\x1B[0m')});
 }
 
-module.exports = writeHtml;
+module.exports = {writeHtml};
 
