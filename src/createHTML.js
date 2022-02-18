24fb6dff12268f3aa85951f2d23cd35eae9be435
kbario <kylebario1@gmail.com>
2022-02-18 09:19:11 +0800

    optimised html creation

--- src/createHTML.js
+++ src/createHTML.js
@@ -1,9 +1,37 @@
 const fs = require('fs');
 
+// function that creates the card
+function createCard(obj) {
+    const lastCard = obj.createHTML()
+    return `<article class="card">
+        <div class="card-header ${obj.getRoleLc()}-header">
+            <h1 class="text-3xl mb-1">${obj.getName()}</h1>
+            <h2 class="text-xl"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path d="M12 14l9-5-9-5-9 5 9 5z" />
+            <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222" />
+          </svg> ${obj.getRole()}</h2>
+        </div>
+        <div class="card-body">
+            <div class="card-info ${obj.getRoleLc()}-info">
+                <div class="card-info-section">
+                    <p class="card-info-section-header">ID</p>
+                    <p>${obj.getId()}</p>
+                </div>
+                <a href="mailto:${obj.getEmail()}" class="card-info-section border-y border-y-slate-600 ${obj.getRoleLc()}-info-section">
+                    <p class="card-info-section-header">Email</p>
+                    <p>${obj.getEmail()}</p>
+                </a>
+                ${lastCard}
+            </div>
+        </div>
+    </article>`
+}
+
 // function that takes an obj and dynamically inputs info into html element
 function createCards(objs){
     return objs.map((obj) => {
-      return obj.createHTML()
+      return createCard(obj)
     }).join('')
 }
 
